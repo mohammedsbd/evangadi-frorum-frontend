d677819bdcb442b3ef6637f704e30baa53de5f77
mohammedsbd <Mohammed1112ok@gmail.com>
2024-10-20 05:18:15 -0700

    updated

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://evangadi-forum-backend-deploy-2-4ved.onrender.com/api",
 });
 
 export default axiosBase;
